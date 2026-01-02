ff6e4b2cf9c06ac29eb2e1a92d4fc559c5e6f007
ayush8 <ayushdayal900@gmail.com>
2026-01-03 02:36:53 +0530

    Allow Amplify domain in CORS

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,7 @@ app.use(cors({
         'http://localhost:5175',
         'http://localhost:5176',
         'https://mahalaxmi-tailoring.vercel.app', // Vercel App
+        'https://main.d10dcrvma98p9c.amplifyapp.com', // AWS Amplify App
         process.env.FRONTEND_URL
     ].filter(Boolean),
     credentials: true // Allow cookies
